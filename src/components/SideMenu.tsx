e25acfc48699ccf7548b1b580033055b67a1a1c4
Viktoriia <beloborodova.viktoriia@gmail.com>
2024-12-19 18:57:54 +0200

    Fix active link at SideMenu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../index.css';
 
 type MenuLink = {
@@ -12,19 +12,17 @@ interface SideMenuProps {
 }
 
 const SideMenu: React.FC<SideMenuProps> = ({ linksList }) => {
-  const location = useLocation();
-  const pathname = location.pathname;
-
   return (
     <aside className='sticky top-7 flex flex-col max-w-80 gap-1 z-10 h-fit self-start'>
       {linksList.map((menu) => {
         const { id, url, title } = menu;
-        const isActive = url === pathname;
+        
         return (
           <NavLink
             key={id}
-            className={`side_link ${isActive ? 'active':''}`}
+            className='side_link'
             to={url}
+            end
           >
             {title}
           </NavLink>
