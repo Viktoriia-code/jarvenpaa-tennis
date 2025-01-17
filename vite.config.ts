8cb30330c4ceda3d2127b1bd5c9711b473e81fee
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-01-17 20:44:15 +0200

    Refactor import path to use alias for pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@pages': '/src/pages', // Make sure it's relative to the root project folder
+    },
+  },
   plugins: [react()],
-  base: "./",
-})
+  base: './',
+});
