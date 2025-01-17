38c7d9186b300b103b1ea8d438f7294c90ac0d62
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-01-17 20:46:26 +0200

    Remove alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@pages': '/src/pages', // Make sure it's relative to the root project folder
-    },
-  },
   plugins: [react()],
   base: './',
 });
