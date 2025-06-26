f895583ebaaa31a0b27db2a8d02c8acef45a322c
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-06-26 14:50:31 +0300

    Move Tournaments images into public folder

--- src/components/GalleryComponent.tsx
+++ src/components/GalleryComponent.tsx
@@ -3,10 +3,10 @@ import 'photoswipe/dist/photoswipe.css';
 
 const GalleryComponent = () => {
   const images = [
-    { src: '/src/assets/images/Tournaments/Näyttökuva 2023-8-13 kello 17.29.51.png', width: 610, height: 700, title: 'Virpi ja Anna' },
-    { src: '/src/assets/images/Tournaments/Näyttökuva 2023-8-13 kello 17.29.59.png', width: 674, height: 1064, title: 'Lauri ja Sampo' },
-    { src: '/src/assets/images/Tournaments/Näyttökuva 2023-8-13 kello 17.29.38.png', width: 734, height: 1078, title: 'Antti ja Noel' },
-    { src: '/src/assets/images/Tournaments/Näyttökuva 2023-8-13 kello 17.30.09.png', width: 1044, height: 918, title: 'Tomi/Heini ja Raija/Martti' },
+    { src: '/images/Tournaments/Näyttökuva 2023-8-13 kello 17.29.51.png', width: 610, height: 700, title: 'Virpi ja Anna' },
+    { src: '/images/Tournaments/Näyttökuva 2023-8-13 kello 17.29.59.png', width: 674, height: 1064, title: 'Lauri ja Sampo' },
+    { src: '/images/Tournaments/Näyttökuva 2023-8-13 kello 17.29.38.png', width: 734, height: 1078, title: 'Antti ja Noel' },
+    { src: '/images/Tournaments/Näyttökuva 2023-8-13 kello 17.30.09.png', width: 1044, height: 918, title: 'Tomi/Heini ja Raija/Martti' },
   ];
 
   return (
