38c3fc3a5fb77b9c526a7084b78d36fdaeb0e1cc
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-07-03 11:39:24 +0300

    Add  'All' location option and add relevant headings

--- src/components/home/Schedule.tsx
+++ src/components/home/Schedule.tsx
@@ -3,12 +3,13 @@ import '../../index.css';
 import CourseCalendar from "../CourseCalendar";
 import { useState } from "react";
 
-type LocationKeys = "Kerava" | "Järvenpää";
+type LocationKeys = "Kerava" | "Järvenpää" | "All";
 
 const Schedule: React.FC = () => {
   const [selected, setSelected] = useState<Record<LocationKeys, boolean>>({
     Kerava: true,
     Järvenpää: true,
+    All: true,
   });
 
   const toggle = (loc: LocationKeys) => {
@@ -21,9 +22,12 @@ const Schedule: React.FC = () => {
         <SectionTitle title="Aikataulu" />
       </div>
       <div className="container">
+        <h3 className='section_title mb-2'>Kevätkauden 2025 valmennusaikataulu</h3>
+        <p className="mt-4 mb-4">Harjoitukset jatkuvat ti 7.1.2025. Sisäkausi loppuu sunnuntaina 22.6.2025.</p>
         <div className="flex gap-x-8 gap-y-6 ">
           <CourseCalendar />
           <div className="flex flex-col gap-4 items-start">
+            <p className="subtitle font-text">Locations:</p>
             {Object.keys(selected).map((loc) => {
               const key = loc as LocationKeys;
               return (
