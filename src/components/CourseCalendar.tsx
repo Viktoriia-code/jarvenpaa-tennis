195712018222f8b8996707d134db97c53a761a02
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-07-03 18:38:00 +0300

    Update event time display and add event data for CourseCalendar

--- src/components/CourseCalendar.tsx
+++ src/components/CourseCalendar.tsx
@@ -9,7 +9,7 @@ const renderEventContent = (arg: EventContentArg) => {
   const timeText = arg.timeText;
   return (
     <>
-      <div>{timeText}</div>
+      <div className="whitespace-nowrap">{timeText}</div>
       <div>{line1}</div>
       <div>{line2}</div>
     </>
@@ -108,10 +108,52 @@ const mapEvents = (rawEvents: RawEvent[], monday: Date): EventInput[] => {
 };
 
 const rawEvents: RawEvent[] = [
-  { id: "1", text: "Aikuiset", day: "Monday", startTime: "10:00", endTime: "11:00", color: "#DE6C54", location: "Kerava 1" },
-  { id: "2", text: "Aikuiset", day: "Tuesday", startTime: "11:00", endTime: "12:00", color: "#4285DA", location: "LS" },
-  { id: "3", text: "Juniors", day: "Wednesday", startTime: "14:00", endTime: "15:00", color: "#79B160", location: "TH" },
-  { id: "4", text: "Juniors", day: "Thursday", startTime: "13:00", endTime: "14:00", color: "#F2C846", location: "Kerava 2" },
+  { id: "1", text: "Aikuiset", day: "Monday", startTime: "12:00", endTime: "13:30", color: "#DE6C54", location: "Best" },
+  { id: "2", text: "Aikuiset", day: "Monday", startTime: "16:00", endTime: "17:00", color: "#DE6C54", location: "Best" },
+  { id: "3", text: "Aikuiset", day: "Monday", startTime: "17:00", endTime: "18:00", color: "#DE6C54", location: "Best" },
+  { id: "4", text: "Aikuiset", day: "Monday", startTime: "18:00", endTime: "19:00", color: "#DE6C54", location: "Best" },
+  { id: "5", text: "Aikuiset", day: "Monday", startTime: "19:00", endTime: "20:00", color: "#DE6C54", location: "Best" },
+  { id: "6", text: "Aikuiset", day: "Monday", startTime: "20:00", endTime: "21:00", color: "#DE6C54", location: "Best" },
+  { id: "7", text: "Juniors", day: "Monday", startTime: "16:00", endTime: "17:00", color: "#4285DA", location: "LS" },
+  { id: "8", text: "Juniors", day: "Monday", startTime: "17:00", endTime: "18:00", color: "#4285DA", location: "LS" },
+  { id: "9", text: "Aikuiset", day: "Monday", startTime: "18:00", endTime: "19:00", color: "#4285DA", location: "LS" },
+  { id: "10", text: "Aikuiset", day: "Monday", startTime: "19:00", endTime: "20:00", color: "#4285DA", location: "LS" },
+  { id: "11", text: "Aikuiset", day: "Tuesday", startTime: "18:00", endTime: "19:00", color: "#DE6C54", location: "Kerava 1" },
+  { id: "12", text: "Aikuiset", day: "Tuesday", startTime: "11:00", endTime: "12:00", color: "#79B160", location: "Kerava 1" },
+  { id: "13", text: "Juniors", day: "Tuesday", startTime: "16:00", endTime: "17:00", color: "#79B160", location: "Kerava 1" },
+  { id: "14", text: "Juniors", day: "Tuesday", startTime: "17:00", endTime: "18:00", color: "#79B160", location: "Kerava 1" },
+  { id: "15", text: "Aikuiset", day: "Tuesday", startTime: "18:00", endTime: "19:00", color: "#79B160", location: "Kerava 2" },
+  { id: "16", text: "Aikuiset", day: "Tuesday", startTime: "19:00", endTime: "20:00", color: "#79B160", location: "Kerava 2" },
+  { id: "17", text: "Aikuiset", day: "Tuesday", startTime: "20:00", endTime: "21:00", color: "#79B160", location: "Kerava 2" },
+  { id: "18", text: "Juniors", day: "Tuesday", startTime: "16:00", endTime: "17:00", color: "#4285DA", location: "LS" },
+  { id: "19", text: "Aikuiset", day: "Tuesday", startTime: "17:00", endTime: "18:00", color: "#4285DA", location: "LS" },
+  { id: "20", text: "Aikuiset", day: "Tuesday", startTime: "18:00", endTime: "19:00", color: "#4285DA", location: "LS" },
+  { id: "21", text: "Aikuiset", day: "Tuesday", startTime: "19:00", endTime: "20:00", color: "#4285DA", location: "LS" },
+  { id: "22", text: "Aikuiset", day: "Tuesday", startTime: "20:00", endTime: "21:00", color: "#4285DA", location: "LS" },
+  { id: "23", text: "Juniors", day: "Tuesday", startTime: "18:00", endTime: "19:00", color: "#F2C846", location: "Best" },
+  { id: "24", text: "Juniors", day: "Wednesday", startTime: "16:00", endTime: "17:00", color: "#DE6C54", location: "TH" },
+  { id: "25", text: "Juniors", day: "Wednesday", startTime: "17:00", endTime: "18:00", color: "#DE6C54", location: "LS" },
+  { id: "26", text: "Aikuiset", day: "Wednesday", startTime: "18:00", endTime: "19:00", color: "#DE6C54", location: "LS" },
+  { id: "27", text: "Aikuiset", day: "Wednesday", startTime: "19:00", endTime: "20:00", color: "#DE6C54", location: "Best" },
+  { id: "28", text: "Juniors", day: "Wednesday", startTime: "15:00", endTime: "16:00", color: "#F2C846", location: "LS" },
+  { id: "29", text: "Juniors", day: "Wednesday", startTime: "16:00", endTime: "17:00", color: "#F2C846", location: "LS" },
+  { id: "30", text: "Juniors", day: "Wednesday", startTime: "18:00", endTime: "19:00", color: "#F2C846", location: "TH" },
+  { id: "31", text: "Juniors", day: "Wednesday", startTime: "15:00", endTime: "16:00", color: "#4285DA", location: "Best" },
+  { id: "32", text: "Aikuiset", day: "Wednesday", startTime: "16:00", endTime: "17:00", color: "#4285DA", location: "Best" },
+  { id: "33", text: "Aikuiset", day: "Wednesday", startTime: "17:00", endTime: "18:00", color: "#4285DA", location: "Best" },
+  { id: "34", text: "Aikuiset", day: "Wednesday", startTime: "18:00", endTime: "19:00", color: "#4285DA", location: "Best" },
+  { id: "35", text: "Aikuiset", day: "Thursday", startTime: "10:00", endTime: "11:00", color: "#DE6C54", location: "LS" },
+  { id: "36", text: "Aikuiset", day: "Thursday", startTime: "17:00", endTime: "18:00", color: "#DE6C54", location: "Kerava 2" },
+  { id: "37", text: "Aikuiset", day: "Thursday", startTime: "18:00", endTime: "19:00", color: "#DE6C54", location: "Kerava 2" },
+  { id: "38", text: "Aikuiset", day: "Thursday", startTime: "19:00", endTime: "20:00", color: "#DE6C54", location: "Kerava 2" },
+  { id: "39", text: "Juniors", day: "Thursday", startTime: "17:00", endTime: "18:00", color: "#4285DA", location: "LS" },
+  { id: "40", text: "Aikuiset", day: "Thursday", startTime: "18:00", endTime: "19:00", color: "#4285DA", location: "LS" },
+  { id: "41", text: "Aikuiset", day: "Thursday", startTime: "20:00", endTime: "21:00", color: "#4285DA", location: "LS" },
+  { id: "42", text: "Juniors", day: "Friday", startTime: "17:00", endTime: "18:00", color: "#DE6C54", location: "LS" },
+  { id: "43", text: "Juniors", day: "Friday", startTime: "16:00", endTime: "17:00", color: "#4285DA", location: "LS" },
+  { id: "44", text: "Juniors", day: "Friday", startTime: "17:00", endTime: "18:00", color: "#4285DA", location: "TH" },
+  { id: "45", text: "Juniors", day: "Friday", startTime: "18:00", endTime: "19:00", color: "#4285DA", location: "LS" },
+  { id: "46", text: "Aikuiset", day: "Friday", startTime: "19:00", endTime: "20:00", color: "#4285DA", location: "TH" },
 ];
 
 const CourseCalendar = (): JSX.Element => {
